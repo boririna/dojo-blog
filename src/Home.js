3cb974bc369eb805db6a2ea88ea4bea50ffa48ec
Irina Borisova <i.a.borisova@gmail.com>
2022-08-16 22:53:59 +0200

    Added bolglist, useEffect function n

--- src/Home.js
+++ src/Home.js
@@ -1,11 +1,11 @@
-import { useState } from "react";
+import { useState, useEffect} from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
   // let name = 'Irina'
 
   // Using a hook use State, hook is a special function that does
-  // certain job
+  // a certain job
   // const [name, setName] = useState('Mario');
   // const [age, setAge] = useState(30);
 
@@ -18,15 +18,35 @@ const Home = () => {
   //   setName('Luigi');
   //   setAge(35);
   // }
-  const [blogs, setBlog] = useState ([
-    { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-    { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-    { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
-  ]);
+  const [blogs, setBlogs] = useState (null);
+  const [isPending, setIsPending] = useState(true);
+
+  // const handleDelete = (id) => {
+  //   const newBlogs = blogs.filter(blog => blog.id !== id);
+  //   setBlogs(newBlogs);
+  // }
+
+  useEffect(() => {
+    setTimeout(() => {
+      fetch('http://localhost:8000/blogs')
+        .then(res => {
+          return res.json();
+        })
+        .then((data => {
+          setBlogs(data);
+          setIsPending(false);
+        }))
+    }, 1000);
+  }, []);
+
   return (
     <div className="home">
-      <BlogList blogs={blogs} title="All Blogs"/>
-      <BlogList blogs={blogs.filter((blog) => blog.author === 'mario')} title="Mario's blogs"/>
+      {isPending && <p>Loading...</p>}
+      {blogs && <BlogList blogs={blogs} title="All Blogs" />}
+
+
+      {/* <BlogList blogs={blogs.filter((blog) => blog.author === 'mario')} title="Mario's blogs"/> */}
+
       {/* <p>{name} is {age} years old</p>
       <button onClick={ handleClick }>Click me</button> */}
 
