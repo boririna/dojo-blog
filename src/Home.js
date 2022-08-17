6b384c0553d6f9addfc6a86af890ee061033bc3c
Irina Borisova <i.a.borisova@gmail.com>
2022-08-17 11:53:31 +0200

    Added Errors catch

--- src/Home.js
+++ src/Home.js
@@ -20,6 +20,7 @@ const Home = () => {
   // }
   const [blogs, setBlogs] = useState (null);
   const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState(null);
 
   // const handleDelete = (id) => {
   //   const newBlogs = blogs.filter(blog => blog.id !== id);
@@ -30,17 +31,27 @@ const Home = () => {
     setTimeout(() => {
       fetch('http://localhost:8000/blogs')
         .then(res => {
+          console.log(res);
+          if(!res.ok) {
+            throw Error('could not fetch the data for that resourse');
+          }
           return res.json();
         })
         .then((data => {
           setBlogs(data);
           setIsPending(false);
+          setError(false);
         }))
+        .catch(err => {
+          setIsPending(false)
+          setError(err.message);
+        })
     }, 1000);
   }, []);
 
   return (
     <div className="home">
+      {error && <div>{error}</div>}
       {isPending && <p>Loading...</p>}
       {blogs && <BlogList blogs={blogs} title="All Blogs" />}
 
